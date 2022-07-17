c11c7c0e6a51c37501286e38df5c3f5d124361cc
Andrii Krokhta <nowayoutta13@gmail.com>
2022-07-17 17:31:46 +0300

    Added PublishService

--- src/main/java/com/example/ticketpublisher/controller/PublishController.java
+++ src/main/java/com/example/ticketpublisher/controller/PublishController.java
@@ -1,14 +1,10 @@
 package com.example.ticketpublisher.controller;
 
 import com.example.ticketpublisher.dto.TicketDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.ticketpublisher.service.PublishService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +14,15 @@ public class PublishController {
 
 	private static final Logger logger = LoggerFactory.getLogger(PublishController.class);
 
-	@Autowired
-	private JmsTemplate jmsTemplate;
+	private final PublishService publishService;
 
-	@Autowired
-	private ObjectMapper mapper;
+	public PublishController(PublishService publishService) {
+		this.publishService = publishService;
+	}
 
 	@PostMapping("/publishMessage")
-	public ResponseEntity<String> publishMessage(@RequestBody TicketDto ticketDto) throws JsonProcessingException {
-		var message = mapper.writeValueAsString(ticketDto);
-		logger.info("Pushing message to mp-ticket-queue. Payload: {}", message);
-		jmsTemplate.convertAndSend("mp-ticket-queue", message);
-		return new ResponseEntity<>("Sent", HttpStatus.OK);
+	public ResponseEntity<Void> publishMessage(@RequestBody TicketDto ticketDto) {
+		publishService.publishBookTicketMessage(ticketDto);
+		return ResponseEntity.accepted().build();
 	}
 }
